27fc475ce1ab79d1a3ebf242136bd9e72d110d87
ItsJonQ <itsjonq@gmail.com>
2015-03-18 19:24:42 -0400

    Adjusted SASS compiler to work with Bourbon

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,9 +152,9 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     sass: {
       options: {
-        sourceMap: true,
-        includePaths: ['bower_components']
-        },
+        sourcemap: true,
+        loadPath: 'bower_components'
+      },
       dist: {
         files: [{
           expand: true,
