763f7ec8771f3fdb9a7e2b4de25fb1cae80b596b
ItsJonQ <itsjonq@gmail.com>
2015-03-19 10:43:18 -0400

    Added helpers and customer attribute merging demo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,77 @@
-(function($){
+(function($){ 'use strict';
 
-  var $toggleBtn = $('#toggle-viewport');
+  // Elements
+  var $toggleView = $('#toggle-viewport');
+  var $toggleAttr = $('#toggle-attributes');
   var $viewport = $('[data-js-toggle="viewport"]');
 
-  $toggleBtn.on('click', function() {
+  var $customerName = $('.js-customer-name');
+  var $customerEmail = $('.js-customer-email');
+
+  // Data
+  var customerData = [
+    {
+      name: 'Colin',
+      email: 'colin@customer.io'
+    },
+    {
+      name: 'John',
+      email: 'john@customer.io'
+    },
+    {
+      name: 'Diana',
+      email: 'diana@customer.io'
+    },
+    {
+      name: 'Stephen',
+      email: 'stephen@customer.io'
+    },
+    {
+      name: 'Alvin',
+      email: 'alvin@customer.io'
+    },
+    {
+      name: 'Henry',
+      email: 'henry@customer.io'
+    },
+    {
+      name: 'Rachel',
+      email: 'rachel@customer.io'
+    },
+    {
+      name: 'Q',
+      email: 'jon@customer.io'
+    },
+    {
+      name: 'Livia',
+      email: 'livia@customer.io'
+    },
+    {
+      name: 'Janet',
+      email: 'janet@customer.io'
+    },
+    {
+      name: 'Abram',
+      email: 'abram@customer.io'
+    }
+  ];
+
+  // fn
+  var getCustomer = function() {
+    return customerData[Math.floor(Math.random()*customerData.length)];
+  };
+
+
+  // Actions
+  $toggleView.on('click', function() {
     $viewport.toggleClass('amp-viewport--mobile');
+  });
+
+  $toggleAttr.on('click', function() {
+    var customer = getCustomer();
+
+    $customerName.html(customer.name);
+    $customerEmail.html(customer.email);
   });
 
 })(jQuery);
